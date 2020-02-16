9f3c70d6c264f539d6f1d11b40039f64891755cf
manoj <manoj.prithvee@gmail.com>
2020-02-16 13:02:38 -0500

    Added ability for subselect and fixed bugs

--- src/com/database/Evaluator.java
+++ src/com/database/Evaluator.java
@@ -5,14 +5,61 @@ import net.sf.jsqlparser.expression.PrimitiveValue;
 import net.sf.jsqlparser.schema.Column;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 
 
 public class Evaluator extends Eval {
-    public Evaluator() {
 
+    private HashMap<String, Integer> schema;
+    private Object[] tuple;
+
+    public Evaluator(HashMap<String, Integer> table, Object[] tuple) {
+        this.schema = table;
+        this.tuple = tuple;
     }
 
     public PrimitiveValue eval(Column c) throws SQLException {
-        return null;
+        String t = "";
+        int columnID = 0;
+        if (c.getTable() != null && c.getTable().getName() != null) {
+            t = c.getTable().getName();
+            if (schema.containsKey(t + "." + c.getColumnName())) {
+                columnID = schema.get(t + "." + c.getColumnName());
+                return (PrimitiveValue) tuple[columnID];
+            } else {
+                for (String key : schema.keySet()) {
+                    String x = key.substring(key.indexOf(".") + 1);
+                    if (x.equals(c.getTable() + "." + c.getColumnName())) {
+                        columnID = schema.get(key);
+                    }
+                }
+                return (PrimitiveValue) tuple[columnID];
+            }
+        } else {
+            if (Global.alias != null && Global.alias.containsKey(c.getColumnName())) {
+                if (schema.containsKey(Global.alias.get(c.getColumnName()).toString()))
+                    columnID = schema.get(Global.alias.get(c.getColumnName()).toString());
+                else if (schema.containsKey(c.getColumnName())) {
+
+                    columnID = schema.get(c.getColumnName());
+                } else {
+                    for (String key : schema.keySet()) {
+                        String x = key.substring(key.indexOf(".") + 1);
+                        if (x.equals(c.getColumnName())) {
+                            columnID = schema.get(key);
+                        }
+                    }
+                }
+                return (PrimitiveValue) tuple[columnID];
+            } else {
+                for (String key : schema.keySet()) {
+                    String x = key.substring(key.indexOf(".") + 1);
+                    if (x.equals(c.getColumnName())) {
+                        columnID = schema.get(key);
+                    }
+                }
+            }
+            return (PrimitiveValue) tuple[columnID];
+        }
     }
 }
