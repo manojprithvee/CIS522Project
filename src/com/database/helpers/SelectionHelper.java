968b7068286eaa7483a523b5da6360c49a25240d
akashsurya156@gmail.com <akashsurya156@gmail.com>
2020-02-17 21:00:36 -0500

    Added Selection operator to the Project, checked everything. Divakar, please make sure of it once again.

--- src/com/database/helpers/SelectionHelper.java
+++ src/com/database/helpers/SelectionHelper.java
@@ -1,24 +1,54 @@
 package com.database.helpers;
 
 
+import com.database.Evaluator;
+import net.sf.jsqlparser.expression.BooleanValue;
+import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.schema.Table;
+
+import java.sql.SQLException;
+import java.util.HashMap;
 
 
 public class SelectionHelper implements HelperImp {
-    public SelectionHelper() {
 
+    HelperImp op;
+    HashMap<String, Integer> schema;
+    Expression condition;
+
+    public SelectionHelper(HelperImp input, HashMap<String, Integer> schema, Expression condition) {
+
+        this.op = input;
+        this.schema = schema;
+        this.condition = condition;
 
     }
 
     @Override
     public void reset() {
-
+        op.reset();
     }
 
 
     @Override
     public Object[] read() {
-
+        Object[] tuple = null;
+        tuple = op.read();
+        if (tuple == null) {
+            return null;
+        }
+        Evaluator eval = new Evaluator(schema, tuple);
+        try {
+            if (((BooleanValue) eval.eval(condition)).getValue()) {
+                return tuple;
+            } else {
+                tuple = read();
+                return tuple;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Exception occured in SelectionOperator.readOneTuple()");
+        }
         return null;
     }
 
